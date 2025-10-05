b5cc65dbb65d7cfee74e46a0bf189dff5aa10d09
guilhermebetzila <guilhermeacessorialtda@gmail.com>
2025-10-05 19:17:50 -0300

    corrige codigo

--- app/api/auth/esqueci-a-senha/route.ts
+++ app/api/auth/esqueci-a-senha/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: Request) {
       data: {
         token,
         userId: user.id,
-        expires,
+        expiresAt: expires, // ✅ corrigido
       },
     })
 
